b4ad951b2382ce7eda10d6bcd68a539bba20f6ea
Jonny <jonik0744clash@email.com>
2026-07-19 19:37:10 +0900

    feat: create homePage Redux configuration

---
+++ src/app/screens/homePage/selector.ts
@@ -0,0 +1,19 @@
+import { createSelector } from "reselect";
+import { AppRootState } from "../../../lib/types/screen";
+
+const selectHomePage = (state: AppRootState) => state.homePage;
+
+export const retrivePopularDishes = createSelector(
+    selectHomePage, 
+    (HomePage) => HomePage.popularDishes
+);
+
+export const retriveNewDishes = createSelector(
+    selectHomePage, 
+    (HomePage) => HomePage.newDishes
+);
+
+export const retriveTopUsers = createSelector(
+    selectHomePage, 
+    (HomePage) => HomePage.topUsers
+);
--- src/lib/types/screen.ts
+++ src/lib/types/screen.ts
@@ -13,7 +13,7 @@ export interface AppRootState {
 /** HOMEPAGE **/
 export interface HomePageState {
     popularDishes: Product[];
-    newDished: Product[];
+    newDishes: Product[];
     topUsers: Member[];
 }
 
