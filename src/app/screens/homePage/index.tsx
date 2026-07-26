966a9176be5fe47049ed7bb327b15106e01867fa
Jonny <jonik0744clash@email.com>
2026-07-26 15:20:00 +0900

    feat: develop product service business logic to retrive data from backend server

--- src/app/screens/homePage/index.tsx
+++ src/app/screens/homePage/index.tsx
@@ -7,12 +7,12 @@ import ActiveUsers from "./ActiveUsers";
 import Events from "./Events";
 import "../../../css/home.css";
 
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { Dispatch } from "@reduxjs/toolkit";
-import { createSelector } from "reselect";
 import { setPopularDishes } from "./slice";
-import { retrievePopularDishes } from "./selector";
 import { Product } from "../../../lib/types/product";
+import ProductService from "../../services/ProductService";
+import { ProductCollection } from "../../../lib/enums/product.enum";
 
 /** REDUX SLICE & SELECTOR */
 
@@ -20,62 +20,30 @@ const actionDispacth = ( dispatch: Dispatch) => ({
   setPopularDishes: (data: Product[] ) => dispatch(setPopularDishes(data)),
 });
 
-const popularDishesRetriever = createSelector(
-  retrievePopularDishes, 
-  (popularDishes) => ({ popularDishes }));
+
 
 
 export default function HomePage() {
 
   const { setPopularDishes } = actionDispacth(useDispatch());
-  const { popularDishes } = useSelector(popularDishesRetriever);
   //Selector: Store => Data
 
   useEffect(() => {
    // Backend server data request => Data
-    const result = [
-    {
-        "_id": "6a3df47fc08d7b6483ab3384",
-        "productStatus": "PROCESS",
-        "productCollection": "DISH",
-        "productName": "Steak",
-        "productPrice": 17,
-        "productLeftCount": 34,
-        "productSize": "LARGE",
-        "productVolume": 1,
-        "productDesc": "New Steak ",
-        "productImage": [],
-        "productView": 0,
-        "createdAt": "2026-06-26T03:39:43.616Z",
-        "updatedAt": "2026-07-15T07:09:13.149Z",
-        "__v": 0,
-        "productViews": 2
-    },
-    {
-        "_id": "6a3df1a3c08d7b6483ab337e",
-        "productStatus": "PROCESS",
-        "productCollection": "DISH",
-        "productName": "Steak",
-        "productPrice": 15,
-        "productLeftCount": 95,
-        "productSize": "NORMAL",
-        "productVolume": 1,
-        "productDesc": "New Steak - New taste - New experience = order now !!!",
-        "productImage": [],
-        "productView": 0,
-        "createdAt": "2026-06-26T03:27:31.049Z",
-        "updatedAt": "2026-07-16T14:42:31.517Z",
-        "__v": 0,
-        "productViews": 5
-    }
-]
-   //Slice: Data => Store
-   //@ts-ignore
-   setPopularDishes(result);
-  }, [])
+    const product = new ProductService ();
+    product.getProducts({
+      page: 1,
+      limit: 4,
+      order: "productViews",
+      productCollection: ProductCollection.DISH,
+    }).then(data => {
+      console.log("data passed here:", data);
+      setPopularDishes(data);
+    }).catch((err) => console.log(err));
 
-  // console.log("popularDishes", popularDishes);
-  console.log(process.env.REACT_APP_API_URL);
+   //Slice: Data => Store
+
+  }, [])
 
   
 
---
+++ src/app/services/ProductService.tsx
@@ -0,0 +1,32 @@
+import axios from "axios";
+import { serverApi } from "../../lib/config";
+import { Product, ProductInquiry } from "../../lib/types/product";
+import { ProductCollection } from "../../lib/enums/product.enum";
+
+class ProductService {
+    private readonly path: string;
+
+    constructor() {
+        this.path = serverApi;
+    }
+
+public async getProducts(input: ProductInquiry): Promise<Product[]> {
+    try {
+        let url= `${this.path}/product/all?order=${input.order}&limit=${input.limit}&page=${input.page}`;
+        if(input.productCollection) 
+            url += `&productCollection=${input.productCollection}`;
+        if(input.search) url += `&search=${input.search}`;
+
+        const result = await axios.get(url);
+        console.log("getProducts:", result);
+
+        return result.data;
+    } catch (err) {
+        console.log("Error, getProducts:", err);
+        throw err;
+    }
+}
+}
+
+
+export default ProductService;
